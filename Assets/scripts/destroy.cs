87aaf2b967b78b1c2ee67537c130d9285b768628
nhafi1 <nauman.hafiz@rga.com>
2016-05-03 00:53:28 -0400

    added destroy and ttl

---
+++ Assets/scripts/destroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroy : MonoBehaviour {
+
+	public float ttl = 10;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Timer ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	IEnumerator Timer() {
+		yield return new WaitForSeconds(ttl);
+		Destroy (gameObject);
+	}
+}
