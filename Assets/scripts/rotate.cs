4fb9b9206f0d5a683ad19dbacf0428c3deefb282
nauman hafiz <nauman.hafiz@gmail.com>
2016-05-03 00:12:59 -0400

    initial commit

---
+++ Assets/scripts/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+    private float multiplier = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * multiplier * Time.deltaTime, Space.World);
+	}
+}
---
+++ Assets/scripts/spawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn : MonoBehaviour {
+
+    public GameObject childObject;
+    public float spawnDelay = 3;
+
+	// Use this for initialization
+	void Start () {
+        if (childObject) {
+            InvokeRepeating("SpawnObject", spawnDelay, spawnDelay);
+        }     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void SpawnObject()
+    {
+        if (childObject)
+        {
+            Instantiate(childObject, transform.position, Random.rotation);
+        }
+    }
+
+}
