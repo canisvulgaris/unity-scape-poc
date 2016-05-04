67a920c46fb5d5bb8123ab62a8f2c5f3777b0fe8
nhafi1 <nauman.hafiz@rga.com>
2016-05-04 01:01:08 -0400

    added terrain scene and main scripts

---
+++ Assets/scripts/MAIN.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MAIN : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		createTerrain ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createTerrain() {
+		//TerrainBlock block = new TerrainBlock (new Vector3(0, 0, 0), 0.1F);
+
+		float blockSize = 0.1F;
+		int gridSize = 5;
+
+		for (float iterX = 0.0F; iterX < gridSize; iterX += blockSize) {
+			for (float iterZ = 0.0F; iterZ < gridSize; iterZ += blockSize) {
+				Vector3 pos = new Vector3 (iterX, 0.0F, iterZ);
+				TerrainBlock block = new TerrainBlock (pos, blockSize);
+			}
+		}
+	}
+}
---
+++ Assets/scripts/TerrainBlock.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainBlock : MonoBehaviour {
+
+	private GameObject block;
+	private float blockX = 1;
+	private float blockY = 1;
+	private float blockZ = 1;
+	private float blockSize;
+	private Vector3 blockPosition;
+	private bool isVisible = true;
+
+	public TerrainBlock(Vector3 pos, float size) {
+		blockPosition = pos;
+		blockSize = size;
+
+		block = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		block.transform.localScale = new Vector3(blockX * size, blockY * size, blockZ * size);
+		block.transform.position = pos;
+
+		Material mat = new Material (Shader.Find("Standard"));
+		mat.color = new Color (0, Random.value, 0);
+
+		//block.AddComponent<MeshRenderer>();
+		block.GetComponent<Renderer> ().material = mat;
+	}
+
+	void setActive(bool set) {
+		block.SetActive (set);
+	}
+
+	public bool IsVisible {
+		get {
+			return isVisible;
+		}
+		set {
+			isVisible = value;
+		}
+	}
+
+	public float BlockSize {
+		get {
+			return blockSize;
+		}
+	}
+
+	public Vector3 BlockPosition {
+		get {
+			return blockPosition;
+		}
+		set {
+			blockPosition = value;
+			block.transform.position = value;
+		}
+	}
+}
