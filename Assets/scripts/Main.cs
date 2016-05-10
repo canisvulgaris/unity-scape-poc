156aa7dc0943d13410106a2045c386891c77f7a4
nauman hafiz <nauman.hafiz@gmail.com>
2016-05-10 02:35:09 -0400

    cleaned up code; added comments

--- Assets/scripts/Main.cs
+++ Assets/scripts/Main.cs
@@ -14,11 +14,13 @@ public class Main : MonoBehaviour {
 	private Vector3[,] _positionArray;
 	private TerrainObject[,] _objArray;
 
-	//private float _count = 0;
-
     private System.DateTime _startTime = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
+    //private float _count = 0;
 
-    // Use this for initialization
+    /*************************************************************** 
+     * Initialization
+     * 
+     ***************************************************************/
     void Start () {
 
         if (_objType == null)
@@ -30,98 +32,93 @@ public class Main : MonoBehaviour {
 		_arrayLength = _arrayIndex + 1;
 
 		_positionArray = new Vector3[_arrayLength, _arrayLength];
-		
 
-        //for (int x = 0; x < _arrayLength; x++)
-        //{
-        //    for (int y = 0; y < _arrayLength; y++)
-        //    {
-        //        _positionArray[x, y] = new Vector3(x, y, 0);
-        //    }
-        //}
-
-		createDiamondTerrain ();
-        Debug.Log("diamond complete. " + (System.DateTime.UtcNow - _startTime).Seconds);
+        //set up the _positionArray height map using the Diamond-Square algorithm 
+		updatePositionUsingDiamondSquare ();
+        createMeshTerrain(_positionArray);
 
         //_objArray = new TerrainObject[_arrayLength, _arrayLength];
         //updateObjectTerrain (_positionArray);
         //createObjectTerrain (_positionArray);
-        createMeshTerrain(_positionArray);
-        Debug.Log("mesh complete. " + (System.DateTime.UtcNow - _startTime).Seconds);
+
     }
 
-    // Update is called once per frame
-    //void Update () {	
-	//}
 
-
+    /***************************************************************
+     * creates a new mesh for the object (does not use terrainobject)
+     * 
+    ***************************************************************/
     void createMeshTerrain(Vector3[,] posArray) {
-
         int length = _arrayLength;
 
-        Vector3[] newVertices = new Vector3[ length * length ];
-        Vector2[] newUV = new Vector2[ length * length ];
-        int[] newTriangles = new int[ (3 * 2 * (length - 1) * (length - 1))];
+        Vector3[] meshVertices = new Vector3[ length * length ];
+        Vector2[] meshUV = new Vector2[ length * length ];
+        int[] meshTriangles = new int[ (3 * 2 * (length - 1) * (length - 1))];
 
+        //create a new mesh 
         Mesh mesh = new Mesh();
         _objType.GetComponent<MeshFilter>().mesh = mesh;
 
-        //set vertices
-        int k = 0;
+        //set up the mesh vertices array
+        int vert_index = 0;
         for (int i = 0; i < length; i++)
         {
             for (int j = 0; j < length; j++)
             {
-                newVertices[k] = _positionArray[i, j];
-                //Debug.Log("newVertices : k : " + k + " - " + newVertices[k]);
-                k++;
+                meshVertices[vert_index] = _positionArray[i, j];
+                vert_index++;
             }
         }
 
-        //set uv
-        int m = 0;
-        for (int n = 0; n < length; n++)
+        //set up the mesh uv array
+        int uv_index = 0;
+        for (int i = 0; i < length; i++)
         {
-            for (int o = 0; o < length; o++)
+            for (int j = 0; j < length; j++)
             {
-                newUV[m] = new Vector2(n, o);
-                m++;
+                meshUV[uv_index] = new Vector2(i, j);
+                uv_index++;
             }
         }
 
-        int z = 0;
+        //set up the mesh triangles array
+        int tri_index = 0;
         for (int x = 0; x < length - 1; x++)
         {
             for (int y = 0; y < length - 1; y++)
             {
-                newTriangles[z] =     (x     * length) + y;
-                newTriangles[z + 1] = ((x+1) * length) + y;
-                newTriangles[z + 2] = (x     * length) + y + 1;
+                //add in first three vertices for mesh
+                meshTriangles[tri_index] =     (x     * length) + y;
+                meshTriangles[tri_index + 1] = ((x+1) * length) + y;
+                meshTriangles[tri_index + 2] = (x     * length) + y + 1;
 
-                newTriangles[z + 3] = ((x+1) * length) + y;
-                newTriangles[z + 4] = ((x+1) * length) + y + 1;
-                newTriangles[z + 5] = (x     * length) + y + 1;
+                //add in second three vertices for mesh
+                meshTriangles[tri_index + 3] = ((x+1) * length) + y;
+                meshTriangles[tri_index + 4] = ((x+1) * length) + y + 1;
+                meshTriangles[tri_index + 5] = (x     * length) + y + 1;
 
-
-                //Debug.Log("count: z : " + z);
-                //Debug.Log("tri: " + newTriangles[z] + ", " + newTriangles[z + 1] + ", " + newTriangles[z + 2] + " -  " + newTriangles[z + 3] + ", " + newTriangles[z + 4] + ", " + newTriangles[z + 5]);
-                //Debug.Log("vert: " + newVertices[newTriangles[z]] + ", " + newVertices[newTriangles[z + 1]] + ", " + newVertices[newTriangles[z + 2]] + " -  " + newVertices[newTriangles[z + 3]] + ", " + newVertices[newTriangles[z + 4]] + ", " + newVertices[newTriangles[z + 5]]);
-
-                z += 6;
+                //bump index by 6 for previous puts
+                tri_index += 6;
             }
         }
 
-        mesh.vertices = newVertices;
-        mesh.uv = newUV;
-        mesh.triangles = newTriangles;
+        mesh.vertices = meshVertices;
+        mesh.uv = meshUV;
+        mesh.triangles = meshTriangles;
 
         mesh.RecalculateNormals();
-        //mesh.Optimize();
+        mesh.Optimize();
 
+        //instantiate object with newly added mesh
+        _objType.AddComponent<MeshCollider>();
         Instantiate(_objType, Vector3.zero, Quaternion.Euler(0, 0, 180));
     }
-    
-	void createFlatObjectTerrain() {
+
+    /***************************************************************
+     * Generate a flat terrain using TerrainObject
+     * 
+     ****************************************************************/
+	void createFlatTerrainUsingObject() {
 		int length = _arrayLength;
 
 		for (int i = 0; i < length; i++) {
@@ -133,7 +130,11 @@ public class Main : MonoBehaviour {
 		}
 	}
 
-	void createObjectTerrain(Vector3[,] posArray) {
+    /***************************************************************
+     * Generate a terrain based on position array using TerrainObject
+     * 
+     ****************************************************************/
+    void createPositionalTerrainUsingObject(Vector3[,] posArray) {
 		int length = _arrayLength;
 
 		for (int i = 0; i < length; i++) {
@@ -145,7 +146,11 @@ public class Main : MonoBehaviour {
 		}
 	}
 
-	void updateObjectTerrain(Vector3[,] posArray) {
+    /***************************************************************
+    * update existing positional terrain using TerrainObject
+    * 
+    ****************************************************************/
+    void updateDynamicTerrainUsingObject(Vector3[,] posArray) {
 		int length = _arrayLength;
 
 		for (int i = 0; i < length; i++) {
@@ -156,25 +161,29 @@ public class Main : MonoBehaviour {
 		}
 	}
 
-	void updateBlockPosition(int i, int j, Vector3 pos) {
+    /***************************************************************
+    * update the position of a TerrainObject
+    * 
+    ****************************************************************/
+    void updateObjectPosition(int i, int j, Vector3 pos) {
         _objArray[i, j].ObjectPosition = pos * _objSize;	
 	}
 
-//	IEnumerator flashBlock(TerrainBlock block, float sec) {
-//		yield return new WaitForSeconds (sec);
-//		if (block.IsVisible) {
-//			block.setColor (Color.red);
-//		}
-//	}
-
-	IEnumerator moveBlock(int i, int j, Vector3 pos, float sec) {
+    /***************************************************************
+    * move a TerrainObject with after delay
+    * 
+    ****************************************************************/
+    IEnumerator moveObject(int i, int j, Vector3 pos, float sec) {
 		yield return new WaitForSeconds (sec);
-		updateBlockPosition (i, j, pos);
+        updateObjectPosition(i, j, pos);
         _objArray[i, j].setColor (new Color(0, 0.75F + (Random.value/4), 0));
 	}
 
-	void createDiamondTerrain() {
-		
+    /***************************************************************
+    * update the positional array using the Diamond Square Algorithim
+    * 
+    ****************************************************************/
+    void updatePositionUsingDiamondSquare() {	
 		int size = _arrayIndex;
 		int length = _arrayLength;
 		float roughness = _terrainRoughness;
@@ -188,7 +197,11 @@ public class Main : MonoBehaviour {
 		split (size, roughness, length);
 	}
 
-	void split(int size, float roughness, int full) {
+    /***************************************************************
+    * Diamond Square Algorithim recursive function 
+    * 
+    ****************************************************************/
+    void split(int size, float roughness, int full) {
 		//Debug.Log ("split() params size: " + size + " roughness: " + roughness + " full: " + full	);
 		int half = size / 2;
 		//float scale = roughness * _blockSize;
@@ -213,7 +226,11 @@ public class Main : MonoBehaviour {
 		split (half, roughness, full);
 	}
 
-	void diamond(int x, int y, int half, float offset, int full) {
+    /***************************************************************
+    * Diamond Square Algorithim recursive function 
+    * 
+    ****************************************************************/
+    void diamond(int x, int y, int half, float offset, int full) {
 		//Debug.Log ("diamond() params x: " + x + " y: " + y + " half: " + half + " offset: " + offset + " full: " + full);	
 		float d1 = (y - half) >= 0 ? _positionArray [x, (y - half)].y : 0;
 		float d2 = (x + half) < full ? _positionArray [(x + half), y].y : 0;
@@ -225,6 +242,10 @@ public class Main : MonoBehaviour {
         //StartCoroutine(moveBlock (x, y, _positionArray [x, y], _count +=0.001F));
     }
 
+    /***************************************************************
+    * Diamond Square Algorithim recursive function 
+    * 
+    ****************************************************************/
     void square(int x, int y, int half, float offset, int full) {
 		//Debug.Log ("square() params x: " + x + " y: " + y + " half: " + half + " offset: " + offset + " full: " + full);
 		float s1 = ((x - half) >= 0 || (y - half) >= 0) ? _positionArray [(x - half), (y - half)].y : 0;
@@ -236,4 +257,27 @@ public class Main : MonoBehaviour {
 		_positionArray [x, y] = new Vector3 (x, average + offset, y);
         //StartCoroutine(moveBlock (x, y, _positionArray [x, y], _count +=0.001F));
     }
+
+
+    /*************************************************************** 
+     * Update the positional Array with Default Values
+     * 
+     ***************************************************************/
+    void updatePositionUsingDefault()
+    {
+        for (int x = 0; x < _arrayLength; x++)
+        {
+            for (int y = 0; y < _arrayLength; y++)
+            {
+                _positionArray[x, y] = new Vector3(x, y, 0);
+            }
+        }
+    }
+
+    //	IEnumerator flashBlock(TerrainBlock block, float sec) {
+    //		yield return new WaitForSeconds (sec);
+    //		if (block.IsVisible) {
+    //			block.setColor (Color.red);
+    //		}
+    //	}
 }
