595381fd9d05579fbb96e0e6022c8b07463a636b
nauman hafiz <nauman.hafiz@gmail.com>
2016-06-26 05:15:29 -0400

    added projectile

---
+++ Assets/scripts/shoot/fireProjectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireProjectile : MonoBehaviour {
+
+    public GameObject _objProj;
+    public float _distance = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _distance);
+
+            position = Camera.main.ScreenToWorldPoint(position);
+            GameObject objFired = Instantiate(_objProj, Camera.main.transform.position, Quaternion.identity) as GameObject;
+            objFired.transform.LookAt(position);
+            Debug.Log(position);
+            objFired.GetComponent<Rigidbody>().AddForce(objFired.transform.forward * 1000);
+        }
+    }
+}
