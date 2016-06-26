711c5ffeb262a51e0af8f4b3312891da791c8216
nauman hafiz <nauman.hafiz@gmail.com>
2016-06-26 16:31:14 -0400

    added boost

--- Assets/scripts/movement/basicMoveLook.cs
+++ Assets/scripts/movement/basicMoveLook.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class basicMoveLook : MonoBehaviour {
 
-	public float lookSpeed = 15.0F;
-	public float moveSpeed = 15.0F;
+	public float lookSpeed = 15.0f;
+	public float moveSpeed = 15.0f;
+    public float boostMultiplier = 3.0f;
 
-	public float  rotationX = 0.0F;
-	public float  rotationY = 0.0F;
+    public float  rotationX = 0.0f;
+	public float  rotationY = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,7 @@ public class basicMoveLook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        
 		rotationX += Input.GetAxis("Mouse X")*lookSpeed;
 		rotationY += Input.GetAxis("Mouse Y")*lookSpeed;
 		rotationY = Mathf.Clamp (rotationY, -90, 90);
@@ -23,7 +25,15 @@ public class basicMoveLook : MonoBehaviour {
 		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
 		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
-		transform.position += transform.forward*moveSpeed*Input.GetAxis("Vertical");
-		transform.position += transform.right*moveSpeed*Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Boost") == 1)
+        {
+            transform.position += transform.forward * moveSpeed * boostMultiplier * Input.GetAxis("Vertical");
+            transform.position += transform.right * moveSpeed * boostMultiplier * Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical");
+            transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal");
+        }        
 	}
 }
