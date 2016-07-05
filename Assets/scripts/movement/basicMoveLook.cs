ed1438feee688a1a1ef8cdabc0ffd73fc26c43fc
nhafi1 <nauman.hafiz@rga.com>
2016-07-04 22:37:53 -0400

    updated camera movement to be on fixed plane up and down

--- Assets/scripts/movement/basicMoveLook.cs
+++ Assets/scripts/movement/basicMoveLook.cs
@@ -3,39 +3,63 @@ using System.Collections;
 
 public class BasicMoveLook : MonoBehaviour {
 
-	public float lookSpeed = 15.0f;
-	public float moveSpeed = 15.0f;
-    public float boostMultiplier = 3.0f;
+	public Vector3 _initialCameraPosition = new Vector3 (107.6f, 36.4f, -0.66f);
+	public Vector3 _currentCameraPosition;
 
-    public float  rotationX = 0.0f;
-	public float  rotationY = 0.0f;
+	public Quaternion _initialCameraRotation = new Quaternion (0.0f, -60.0f, 0.0f, 0.0f);
+	public Quaternion _currentCameraRotation;
+
+	public float _defaultLookSpeed = 5.0f;
+	public float _defaultMoveSpeed = 1.0f;
+
+	public float _freeLookSpeed = 5.0f;
+	public float _freeMoveSpeed = 1.0f;
+    public float _boostMultiplier = 3.0f;
 
 	// Use this for initialization
 	void Start () {
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
+		transform.position = _initialCameraPosition;
+		_currentCameraPosition = _initialCameraPosition;
+
+		transform.rotation = _initialCameraRotation;
+		_currentCameraRotation = _initialCameraRotation;
+
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         Cursor.lockState = CursorLockMode.Locked;
 
-        rotationX += Input.GetAxis("Mouse X")*lookSpeed;
-		rotationY += Input.GetAxis("Mouse Y")*lookSpeed;
-		rotationY = Mathf.Clamp (rotationY, -90, 90);
 
-		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+		//rotationY = Mathf.Clamp (rotationY, -90, 90);
+		//add rotation limits
+		//add position limits
 
-        if (Input.GetAxis("Boost") == 1)
-        {
-            transform.position += transform.forward * moveSpeed * boostMultiplier * Input.GetAxis("Vertical");
-            transform.position += transform.right * moveSpeed * boostMultiplier * Input.GetAxis("Horizontal");
-        }
-        else
-        {
-            transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical");
-            transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal");
-        }        
+		transform.localRotation = Quaternion.AngleAxis(_currentCameraRotation.x, Vector3.up);
+		transform.localRotation *= Quaternion.AngleAxis(_currentCameraRotation.y, Vector3.left);
+
+		//transform.position += transform.forward * _defaultMoveSpeed * Input.GetAxis("Vertical");
+		transform.position = _currentCameraPosition;
+		transform.position += transform.right * _defaultMoveSpeed * Input.GetAxis("Horizontal");
+		transform.Translate(0, 0, _defaultMoveSpeed * Input.GetAxis("Vertical"), Space.World);
+		_currentCameraPosition = transform.position;
+
+//      rotationX += Input.GetAxis("Mouse X")*lookSpeed;
+//		rotationY += Input.GetAxis("Mouse Y")*lookSpeed;
+//		rotationY = Mathf.Clamp (rotationY, -90, 90);
+
+//		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+//		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+//        if (Input.GetAxis("Boost") == 1)
+//        {
+//            transform.position += transform.forward * moveSpeed * boostMultiplier * Input.GetAxis("Vertical");
+//            transform.position += transform.right * moveSpeed * boostMultiplier * Input.GetAxis("Horizontal");
+//        }
+//        else
+//        {
+//            transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical");
+//            transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal");
+//        }        
 	}
 }
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # SCAPE_poc
 
 # in progress
-- add collider to the sphere projectile
-- decrease height of terrain based on collider
+- add explosion to the sphere collision with terrain
+- do not let south border wall interfere with projectiles
 
 # todo
-- 
+- limit the movement of camera past certain point
+
+#notes
+camera pos
+p 8.2/40.8/42.6
+r 67.7 89.7 0
+p 107.6 36.4 -0.66
