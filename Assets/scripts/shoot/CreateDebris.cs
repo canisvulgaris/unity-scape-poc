d36f68c62550eb0b874b109b279c40f340f9990c
nauman hafiz <nauman.hafiz@gmail.com>
2016-07-10 17:46:48 -0400

    added smoke particle

--- Assets/scripts/shoot/CreateDebris.cs
+++ Assets/scripts/shoot/CreateDebris.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class CreateDebris : MonoBehaviour {
 
     public GameObject _debrisParticleSystemObj;
+    public GameObject _smokeParticleSystemObj;
 
     private GameObject _projectileParent;
 	private GameObject _debrisObj;
+    private GameObject _smokeObj;
 
     // Use this for initialization
     void Start () {
@@ -22,5 +24,9 @@ public class CreateDebris : MonoBehaviour {
 		_debrisObj = Instantiate (_debrisParticleSystemObj, transform.position + new Vector3(0, -1, 0), Quaternion.Euler(-90, 0, 0)) as GameObject;
 		_debrisObj.transform.parent = _projectileParent.transform;
         Destroy(_debrisObj.gameObject, 10.0f);
-	}
+
+        _smokeObj = Instantiate(_smokeParticleSystemObj, transform.position + new Vector3(0, -1, 0), Quaternion.Euler(-90, 0, 0)) as GameObject;
+        _smokeObj.transform.parent = _projectileParent.transform;
+        Destroy(_smokeObj.gameObject, 10.0f);
+    }
 }
