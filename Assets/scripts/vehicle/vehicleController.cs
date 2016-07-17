0e93e1bc179c124ee4f5db1400b98bc8f25b4134
nauman hafiz <nauman.hafiz@gmail.com>
2016-07-17 01:59:57 -0400

    added visual wheels

--- Assets/scripts/vehicle/vehicleController.cs
+++ Assets/scripts/vehicle/vehicleController.cs
@@ -2,11 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public GameObject leftWheelObject;
+    public GameObject rightWheelObject;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
+
 public class vehicleController : MonoBehaviour
 {
     public List<AxleInfo> axleInfos; // the information about each individual axle
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider, GameObject wheelObject)
+    {
+        if (wheelObject == null)
+        {
+            Debug.LogError("wheelObject not found");
+            return;
+        }
+
+        Transform visualWheel = wheelObject.transform;
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
 
     public void FixedUpdate()
     {
@@ -25,15 +56,8 @@ public class vehicleController : MonoBehaviour
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
             }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel, axleInfo.leftWheelObject);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel, axleInfo.rightWheelObject);
         }
     }
-}
-
-[System.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
 }
