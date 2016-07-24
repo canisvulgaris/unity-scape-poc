79226b5bc89bdc094b3f7cabadf2fec30d761c81
nauman hafiz <nauman.hafiz@gmail.com>
2016-07-24 15:28:47 -0400

    tweaked terrain; added space for help

--- Assets/scripts/gui/guiHelpBtn.cs
+++ Assets/scripts/gui/guiHelpBtn.cs
@@ -12,6 +12,14 @@ public class guiHelpBtn : MonoBehaviour {
         helpText.gameObject.SetActive(false);
     }
 
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ToggleHelpText();
+        }
+    }
+
     // Update is called once per frame
     public void ToggleHelpText() {
         if (helpText.gameObject.activeSelf) {
